e3bbca6331e22132566fd4b2f40aa2b3d064737c
Mohammed Al-Asfar <elasfarc@gmail.com>
2020-11-18 13:32:00 +0200

    Add linters workflow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,39 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-    def new
-        @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    # @user = User.new(username: params[:user_name], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      p '====ooooo======'
+      render :new
     end
+  end
 
-    def create
+  def edit
+    @user = User.find(params[:id])
+  end
 
-        #@user = User.new(username: params[:user_name], email: params[:email], password: params[:password])
-        @user = User.new(user_params)
-        if @user.save 
-            redirect_to new_user_path
-        else
-            p '====ooooo======'
-            render :new
-        end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      p '====ooooo======'
+      render :edit
     end
+  end
 
-    def edit
-        @user = User.find(params[:id])
-    end
+  private
 
-    def update
-        
-        @user = User.find(params[:id])
-        @user.update(user_params)
-        if @user.save 
-            redirect_to @user
-        else
-            p '====ooooo======'
-            render :edit
-        end
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:username, :email, :password)
-    end
-
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
-
-
-
-
